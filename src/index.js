fec0d1b3752660f027050bbc58feebbe89240f60
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2023-10-27 09:15:28 -0400

    ci: Release v0.0.9

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ const run = async () => {
     if (report.valid) {
       core.info("All OK");
     } else {
-      core.info(stringify(report.errors.map(({ message }) => message)));
+      core.info(stringify(report));
+      console.log(report.errors.map(({ message }) => message));
       core.setFailed(`Action failed: Improper title/description format.`);
     }
     return report;
@@ -36,7 +37,6 @@ const run = async () => {
 (async () => {
   try {
     const result = await run();
-    console.log(result);
   } catch (err) {
     core.info(err);
     core.setFailed(`Action failed with error ${err}`);
